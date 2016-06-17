3cd78b0b7e46a8d7552619ea20c4199045b8271c
Pieter Danneels <pieter.danneels@telenet.be>
2016-06-17 14:51:40 +0200

    documented scraper and PEP8

--- scraper.py
+++ scraper.py
@@ -1,26 +1,37 @@
+"""
+    Flight Radar 24 scraper
+    Connects to a mongoDB database and dumps fligts in there
+
+    By: Junzi Sun, Pieter Danneels
+    Summer 2016
+
+"""
+
 import requests
-#import json
 import time
-#import threading
 import pymongo
 from time import strftime, gmtime
 
 def read_ac_data(data):
+    """
+    Reads aircraft data from JSON format
+
+    """
     if len(data) != 18:
         return None
-    ac = {}
-    ac['icao'] = data[0]
-    ac['loc'] = {'lng': data[2], 'lat': data[1]}
-    ac['hdg'] = data[3]
-    ac['alt'] = data[4]
-    ac['spd'] = data[5]         # horizontal speed
-    ac['mdl'] = data[8]
-    ac['regid'] = data[9]       # Registration ID
-    ac['ts'] = data[10]
-    ac['from'] = data[11]
-    ac['to'] = data[12]
-    ac['roc'] = data[15]        # vertical rate
-    return ac
+    aircraft = {}
+    aircraft['icao'] = data[0]          # ICAO code
+    aircraft['loc'] = {'lng': data[2], 'lat': data[1]} # Longitude, Latitude [deg]
+    aircraft['hdg'] = data[3]           # Heading [deg]
+    aircraft['alt'] = data[4]           # Altitude [ft]
+    aircraft['spd'] = data[5]           # Horizontal speed [knots]
+    aircraft['mdl'] = data[8]           # Aircraft model
+    aircraft['regid'] = data[9]         # Registration ID
+    aircraft['ts'] = data[10]           # Time stamp
+    aircraft['from'] = data[11]         # Origin [IATA]
+    aircraft['to'] = data[12]           # Destination [IATA]
+    aircraft['roc'] = data[15]          # Rate of climb [ft/min]
+    return aircraft
 
 EHAM = [57, 47, 0, 10]
 
@@ -32,66 +43,67 @@ DB = 'fr24'
 COLL = strftime("%Y_%m_%d", gmtime())
 # https://docs.mongodb.com/manual/reference/connection-string/
 MCONNECTIONSTRING = "mongodb://"+USERNAME+":"+PASSWORD+"@"+HOST+":"+PORT+"/"+DB
-# Connection to Mongo DB
-try:
-    mconn=pymongo.MongoClient(MCONNECTIONSTRING)
+
+try: # Connection to Mongo DB
+    MCONN = pymongo.MongoClient(MCONNECTIONSTRING)
     print "Connected successfully."
 except pymongo.errors.ConnectionFailure, e:
-   print "Could not connect to MongoDB: %s" % e 
-mdb = mconn[DB]
-mcoll = mdb['test']
+    print "Could not connect to MongoDB: %s" % e
 
-base_url = "http://lhr.data.fr24.com/zones/fcgi/feed.js?faa=1&mlat=1&flarm=0" \
+MDB = MCONN[DB]
+MCOLL = MDB[COLL]
+
+BASE_URL = "http://lhr.data.fr24.com/zones/fcgi/feed.js?faa=1&mlat=1&flarm=0" \
     "&adsb=1&gnd=1&air=1&vehicles=0&estimated=0&maxage=0&gliders=0&stats=1"
-url = base_url + "&bounds=" + ','.join(str(i) for i in EHAM)
+URL = BASE_URL + "&bounds=" + ','.join(str(i) for i in EHAM)
 
-r_session = requests.Session()
-r_session.headers.update({'User-Agent': 'Mozilla/5.0'})
+RSESSION = requests.Session()
+RSESSION.headers.update({'User-Agent': 'Mozilla/5.0'})
 
 # in memory status for fast checking
 tcache = {}
 data = []
 
 while True:
-    r = r_session.get(url)
-    print r.status_code
-    
-    if r.status_code != 200:
-        time.sleep(2)   # We do not want to flood the fr24 server with requests
-        continue
-        
-    try:
-        res = r.json()
-    except:
+    httpget = RSESSION.get(URL)
+
+    if httpget.status_code != 200:
+        print "http error: " + httpget.status_code
+        time.sleep(2) # anti fr24 flood
         continue
 
-    if len(res) < 3: # why?
+    try:
+        fr24data = httpget.json()
+    except:
+        print ""
+        time.sleep(2) # anti fr24 flood
+        continue
+
+    if len(fr24data) < 3:
+        time.sleep(2) # anti fr24 flood
         continue
 
     # remove some fields
-    if 'version' in res:
-        del res['version']
+    if 'version' in fr24data:
+        del fr24data['version']
+    if 'full_count' in fr24data:
+        del fr24data['full_count']
 
-    if 'full_count' in res:
-        del res['full_count']
+    for key, val in fr24data.iteritems():
+        aircraft = read_ac_data(val)
 
-    # print len(res)
-
-    for key, val in res.iteritems():
-        ac = read_ac_data(val)
-
-        if not ac:
+        if not aircraft:
             continue
         else:
-            ac['fr24'] = key
+            aircraft['fr24'] = key
 
-        if ac['to'] != 'AMS' and ac['from'] != 'AMS':
+        if aircraft['to'] != 'AMS' and aircraft['from'] != 'AMS':
             continue
-        
-        if key in tcache and tcache[key] == ac['ts']:
+
+        if key in tcache and tcache[key] == aircraft['ts']:
             continue
         else:
-            mcoll.insert(ac)
-            tcache[key] = ac['ts']
-            
+            MCOLL.insert(aircraft)
+            tcache[key] = aircraft['ts']
+
     time.sleep(2)
